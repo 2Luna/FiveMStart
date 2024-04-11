96b45438108f9ff946733fa3a4836b6f963610b1
Luna <47433349+2Luna@users.noreply.github.com>
2024-04-11 07:44:52 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 # start a new screen session in the background named 'FivemServer'
-screen -dmS FivemServer
+screen -dmS FXServer
 
 # run server within the screen session
-screen -S FivemServer -X stuff "exec $SCRIPTPATH/alpine/opt/cfx-server/ld-musl-x86_64.so.1 \
+screen -S FXServer -X stuff "exec $SCRIPTPATH/alpine/opt/cfx-server/ld-musl-x86_64.so.1 \
     --library-path \"$SCRIPTPATH/alpine/usr/lib/v8/:$SCRIPTPATH/alpine/lib/:$SCRIPTPATH/alpine/usr/lib/\" -- \
     $SCRIPTPATH/alpine/opt/cfx-server/FXServer +set citizen_dir $SCRIPTPATH/alpine/opt/cfx-server/citizen/ $*
 "
